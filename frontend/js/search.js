2436e639f344dfeb5d35bd8a95f41236e237049f
quachck <quachddavid@gmail.com>
2023-06-22 16:15:20 +1000

    Added health and diet filters

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,31 +1,64 @@
 const APP_ID = "4262aadd";
 const API_KEY = "4b7086bb3c6f33c3cd914e1948631c5f";
 
+
 $(document).ready(function () {
   $("#searchButton").click(async function () {
     const recipeName = $("#recipeInput").val().trim();
 
-    if (recipeName) {
-      const recipes = await fetchRecipes(recipeName);
-      displayRecipes(recipes);
+    // Get the values of the checked health and diet filters
+    const healthFilters = $("input[name=health]:checked")
+      .map(function () {
+        return this.value;
+      })
+      .get()
+      .join("&");
+
+    const dietFilters = $("input[name=diet]:checked")
+      .map(function () {
+        return this.value;
+      })
+      .get()
+      .join("&");
+
+    // Validate if at least one search option is filled
+    if (!recipeName && !healthFilters && !dietFilters) {
+      $("#recipeInput").attr(
+        "placeholder",
+        "Please fill in at least 1 search option"
+      );
+      return;
     }
+
+    const recipes = await fetchRecipes(recipeName, healthFilters, dietFilters);
+    displayRecipes(recipes);
   });
 });
 
-async function fetchRecipes(name) {
+async function fetchRecipes(name, healthFilters, dietFilters) {
+  let baseURL = `https://api.edamam.com/search?q=${name}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=10`;
+
+  if (healthFilters) {
+    baseURL += `&health=${healthFilters}`;
+  }
+
+  if (dietFilters) {
+    baseURL += `&diet=${dietFilters}`;
+  }
+
   try {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${name}&app_id=${APP_ID}&app_key=${API_KEY}&from=0&to=10`
-    );
+    const response = await fetch(baseURL);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     const data = await response.json();
     return data.hits;
   } catch (error) {
     console.error("There was a problem with the fetch operation: ", error);
   }
 }
+
 
 function displayRecipes(recipes) {
   $("#recipeContainer").empty();
